3f6a6c69766715a1b454fe2b1eebcf0c76d2eac6
xzidx <c.samnang.de@gmail.com>
2026-02-12 14:08:34 +0700

    Set Up Project

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -4,7 +4,7 @@
       <h1 class="text-xl font-bold">My Nuxt App</h1>
       <ul class="flex gap-4">
         <li><NuxtLink to="/">Home</NuxtLink></li>
-        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="../about">About</NuxtLink></li>
         <li><NuxtLink to="/contact">Contact</NuxtLink></li>
       </ul>
     </nav>
